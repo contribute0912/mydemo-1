939cd3b5d06e8f62b3a19c948202bd29022683e9
contribute0912 <contribute0912@gmail.com>
2025-12-09 19:06:30 +0100

    Update FileDemo.py

--- FileDemo.py
+++ FileDemo.py
@@ -1,10 +1,15 @@
-#Added Fist verison of the copy
+
 from datetime import datetime
 from zoneinfo import ZoneInfo  # Available in Python 3.9+
 
-# Use Europe/Amsterdam to get CET/CEST automatically depending on the date
-tz = ZoneInfo("Europe/Amsterdam")
-now = datetime.now(tz)
+# Time zones
+tz_amsterdam = ZoneInfo("Europe/Amsterdam")
+tz_india = ZoneInfo("Asia/Kolkata")
+
+# Current times
+now_amsterdam = datetime.now(tz_amsterdam)
+now_india = datetime.now(tz_india)
 
 print("Hello Ram")
-print(f"Time is now {now:%Y-%m-%d %H:%M:%S %Z}")
+print(f"Time in Amsterdam: {now_amsterdam:%Y-%m-%d %H:%M:%S %Z}")
+print(f"Time in India:     {now_india:%Y-%m-%d %H:%M:%S %Z}")
