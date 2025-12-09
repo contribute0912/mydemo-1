cfeba9d8579f5f82d37190ba40b9ccbcb278d68d
Ramanjaneyulu Janapati <ramanjaneyulu.janapati@ing.com>
2025-12-09 17:36:40 +0100

    first commit

---
+++ FileDemo.py
@@ -0,0 +1,10 @@
+
+from datetime import datetime
+from zoneinfo import ZoneInfo  # Available in Python 3.9+
+
+# Use Europe/Amsterdam to get CET/CEST automatically depending on the date
+tz = ZoneInfo("Europe/Amsterdam")
+now = datetime.now(tz)
+
+print("Hello Ram")
+print(f"Time is now {now:%Y-%m-%d %H:%M:%S %Z}")
